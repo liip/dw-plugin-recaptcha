23b53d70f4fbda01b001cac3b628cc0182d51592
dryoo <dryoo@live.com>
2014-10-01 09:59:06 +0900

    Create setting.php

---
+++ lang/ko/setting.php
@@ -0,0 +1,10 @@
+<?PHP
+
+
+$lang['publickey'] = "reCAPTCHA 공개 키 (<a href='http://www.recaptcha.net/api/getkey?app=dokuwiki' target='_blank' title='http://recaptcha.net'>http://recaptcha.net/</a>) "; 
+$lang['privatekey'] = "reCAPTCHA 개인 키 (<a href='http://www.recaptcha.net/api/getkey?app=dokuwiki' target='_blank' title='http://recaptcha.net'>http://recaptcha.net/</a>) "; 
+$lang['theme'] = '테마'; 
+$lang['lang'] = 'reCAPTCHA를 위한 언어 (비어있는 경우, 도쿠위키의 언어 설정을 가져오려고 시도합니다)'; 
+$lang['regprotect'] = '등록양식을 보호'; 
+$lang['editprotect'] = '페이지편집양식을 보호'; 
+$lang['forusers'] = '로그인한 사용자도 reCAPTCHA 사용';
